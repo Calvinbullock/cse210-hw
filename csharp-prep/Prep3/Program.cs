c06c055edfbc0b9fe896d1d52bd20f07fdcd1a53
Calvin Bullock <calvinnbullock@gmail.com>
2023-04-24 11:07:35 -0600

    prep2 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,47 @@ using System;
 
 class Program
 {
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Random randomGenerator = new Random();
+        // int MagicNum = randomGenerator.Next(1, 11);
+
+
+        // Takes input to set users magic number 
+        Console.Write("What is the magic number? ");
+        string magicNumInput = Console.ReadLine();
+        int magicNum = int.Parse(magicNumInput);
+
+        // Brings scope of guess outside of the do while loop
+        int guess;
+
+        // Loops until user guesses the right number.
+        do
+        {
+            // Takes input for users guess 
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+            // Checks if user guess is too hight ot to low 
+            if (magicNum < guess)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+
+            }
+        } while (guess != magicNum);
+
     }
 }
