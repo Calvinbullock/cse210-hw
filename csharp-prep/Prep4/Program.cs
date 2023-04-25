8234417b269936a88b61bb7e7f59ad090c5ea2b3
Calvin Bullock <calvinnbullock@gmail.com>
2023-04-25 07:48:24 -0600

    Finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,6 +1,17 @@
+/*
+Calvin Bullock
+April 25th
+Prep4
+
+This program asks the user to input N numbers and will keep asking
+until 0 is enterd. It will then compute the average, max int and sum
+of the enterd numbers excluding the last 0.
+
+*/
+
+
 // using System;
 using System.Collections.Generic;
-
 
 class Program
 {
@@ -22,7 +33,7 @@ class Program
     {
         float average = 0;
         int sum = ListSum(numsList);
-        average = (float)sum / numsList.Count;
+        average = (float)sum / (numsList.Count - 1);
         return average;
     }
 
@@ -44,25 +55,35 @@ class Program
         // Insalizes numIn for use outside the scope of the do while loop.
         int numIn;
 
-        // Test case ------------
-        numsList.Add(18);
-        numsList.Add(36);
-        numsList.Add(2);
-        numsList.Add(48);
-        numsList.Add(6);
-        numsList.Add(12);
-        numsList.Add(9);
-        numsList.Add(0);
-
         // Loops until the User enters 0.
-        // do
-        // {
-        //     // Takes input from user parses it to a int and adds it to numsList.
-        //     Console.Write("Enter number, please: ");
-        //     string numInput = Console.ReadLine();
-        //     numIn = int.Parse(numInput);
-        //     numsList.Add(numIn);
-        // } while (numIn != 0);
+        do
+        {
+            // Takes input from user parses it to a int and adds it to numsList.
+            Console.Write("Enter number, please: ");
+            string numInput = Console.ReadLine();
+            numIn = int.Parse(numInput);
+            numsList.Add(numIn);
+        } while (numIn != 0);
+
+        // Test case ------------
+        // numsList.Add(18);
+        // numsList.Add(36);
+        // numsList.Add(2);
+        // numsList.Add(48);
+        // numsList.Add(6);
+        // numsList.Add(12);
+        // numsList.Add(9);
+        // numsList.Add(0);
+
+        // numsList.Add(3);
+        // numsList.Add(5);
+        // numsList.Add(7);
+        // numsList.Add(3);
+        // numsList.Add(2);
+        // numsList.Add(-1);
+        // numsList.Add(-4);
+        // numsList.Add(-8);
+        // numsList.Add(0);
 
         // Prints out the sum, avrage and largest int of numsList.
         Console.WriteLine($"The sum is: {ListSum(numsList)}");
