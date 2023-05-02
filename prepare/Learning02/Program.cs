f2e0fd80c7b2d96c6f69b715a1a9a85e38337647
Calvinbullock <calvinnbullock@gamil.com>
2023-05-02 09:11:57 -0600

    removed list bugs from program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Creat new instance of the Job class
+        Job job1 = new Job();
+        job1._jobTitle = "bob";
+        job1._company = "tim";
+        job1._startYear = 23;
+        job1._endYear = 23;
+
+        // Creat new instance of the Job class
+        Job job2 = new Job();
+        job1._jobTitle = "bob";
+        job1._company = "tim";
+        job1._startYear = 23;
+        job1._endYear = 23;
+
+        // Creats an instance of Resume
+        
+        Resume resume1 = new Resume();
+        resume1._name = "Jammy Tims";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        // job1.Display();
+
     }
 }
