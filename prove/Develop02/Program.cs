96440190e36c5cc0dc529be67bd9d6d0928d1fe1
Calvinbullock <calvinnbullock@gamil.com>
2023-05-08 14:46:52 -0600

    created test run for journal and Entry classes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Develop02
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            Entry myEntry = new Entry();
+            myEntry.Store("What is my name?", "Calvin Bullock", "8 may 2023");
+            
+            Entry yourEntry = new Entry();
+            yourEntry.Store("What is your name?", "Matt", "8 may 2023");
+
+            Journal journal = new Journal();
+            journal.StoreEntry(myEntry);
+            journal.StoreEntry(yourEntry);
+
+            List<Entry> entries = journal.GetAllEntrys();
+            foreach (Entry entry in entries)
+            {
+                string message = entry.GetAsString();
+                Console.WriteLine(message);
+            }
+        }
     }
 }
