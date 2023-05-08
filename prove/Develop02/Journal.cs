96ec5797fda6a15d101fb05b2dff118b00abe01c
Calvinbullock <calvinnbullock@gamil.com>
2023-05-08 14:47:08 -0600

    created journal and entry classes

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+namespace Develop02
+{
+    ///<summary> 
+    // The responibility of am journal is to store instances of the entry class
+    ///</summary> 
+    class Journal
+    {
+
+        public List<Entry> entries;
+
+        public Journal() {
+            entries = new List<Entry>();
+        }
+
+        public List<Entry> GetAllEntrys()
+        {
+            return entries;
+        }
+
+        public void StoreEntry(Entry entry)
+        {
+            if (!entries.Contains(entry))
+            {
+                entries.Add(entry);
+            }
+        }
+    }
+}
