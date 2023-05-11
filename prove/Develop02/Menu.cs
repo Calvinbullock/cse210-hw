1a30861bc36fbe45d73d3384949dbccb56318138
Calvinbullock <calvinnbullock@gamil.com>
2023-05-11 14:49:42 -0600

    started the Menu class

---
+++ prove/Develop02/Menu.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Develop02
+{
+    class Menu
+    {
+        List<string> menuItems = new List<string>()
+                    {
+                        "1. Write",
+                        "2. Display",
+                        "3. Load",
+                        "4. Save",
+                        "5. Quit"                    
+                    };
+    }
+}
