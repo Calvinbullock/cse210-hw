843488fd57ee8a255b2a59ad53fa351324850500
Calvinbullock <calvinnbullock@gamil.com>
2023-05-11 14:50:32 -0600

    claend up program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,31 +1,36 @@
 using System;
 
-class Program
+namespace Develop02
 {
-    static void Main(string[] args)
+    class Program
     {
-        // Creat new instance of the Job class
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer ";
-        job1._company = "Microsoft";
-        job1._startYear = 2001;
-        job1._endYear = 2007;
+        static void Main(string[] args)
+        {
+            // Creat new instance of the Job class
+            Job job1 = new Job();
+            job1._jobTitle = "Software Engineer ";
+            job1._company = "Microsoft";
+            job1._startYear = 2001;
+            job1._endYear = 2007;
 
-        // Creat new instance of the Job class
-        Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2019;
-        job2._endYear = 2023;
+            // Creat new instance of the Job class
+            Job job2 = new Job();
+            job2._jobTitle = "Manager";
+            job2._company = "Apple";
+            job2._startYear = 2019;
+            job2._endYear = 2023;
 
-        // Creats an instance of Resume
-        
-        Resume resume1 = new Resume();
-        resume1._name = "Jammy Tims";
-        resume1._jobs.Add(job1);
-        resume1._jobs.Add(job2);
-        
-        resume1.Display();
+            // Creats an instance of Resume
 
+            Resume resume1 = new Resume();
+            resume1._name = "Jammy Tims";
+            resume1._jobs.Add(job1);
+            resume1._jobs.Add(job2);
+
+            resume1.Display();
+
+            
+
+        }
     }
 }
