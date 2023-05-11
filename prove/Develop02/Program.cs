1675796dde31f3f3a66b1ce6560e55a5d7f8313d
Calvinbullock <calvinnbullock@gamil.com>
2023-05-11 14:50:55 -0600

    added a prompts test case to program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,22 +8,27 @@ namespace Develop02
     {
         static void Main(string[] args)
         {
-            Entry myEntry = new Entry();
-            myEntry.Store("What is my name?", "Calvin Bullock", "8 may 2023");
+            // Entry myEntry = new Entry();
+            // myEntry.Store("What is my name?", "Calvin Bullock", "8 may 2023");
             
-            Entry yourEntry = new Entry();
-            yourEntry.Store("What is your name?", "Matt", "8 may 2023");
+            // Entry yourEntry = new Entry();
+            // yourEntry.Store("What is your name?", "Matt", "8 may 2023");
 
-            Journal journal = new Journal();
-            journal.StoreEntry(myEntry);
-            journal.StoreEntry(yourEntry);
+            // Journal journal = new Journal();
+            // journal.StoreEntry(myEntry);
+            // journal.StoreEntry(yourEntry);
 
-            List<Entry> entries = journal.GetAllEntrys();
-            foreach (Entry entry in entries)
-            {
-                string message = entry.GetAsString();
-                Console.WriteLine(message);
-            }
+            // List<Entry> entries = journal.GetAllEntrys();
+            // foreach (Entry entry in entries)
+            // {
+            //     string message = entry.GetAsString();
+            //     Console.WriteLine(message);
+            // }
+
+            // Test for prompts class
+            Prompts prompt = new Prompts();
+            string randPrompt = prompt.GetRandomPrompt();
+            Console.WriteLine($"{randPrompt}");
         }
     }
 }
