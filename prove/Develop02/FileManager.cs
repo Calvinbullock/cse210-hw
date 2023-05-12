43b09061b49cbcfdb3b9c3f8be37042badee3e1b
Calvinbullock <calvinnbullock@gamil.com>
2023-05-12 07:49:28 -0600

    FileManager class is in working order

--- prove/Develop02/FileManager.cs
+++ prove/Develop02/FileManager.cs
@@ -7,36 +7,59 @@ namespace Develop02
 {
     class FileManager
     {
-        public FileManager()
+        public FileManager() { }
+        public void saveToFile(string fileName, string fileContent)
         {
+            try
+            {
+                //Open the File
+                StreamWriter stream = new StreamWriter(fileName, true, Encoding.ASCII);
 
+                // Writes content to file
+                stream.WriteLine(fileContent);
+
+                //close the file
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message); // Not sure on error message yet..
+            }
+            finally { } // Not sure if i need something here or not
         }
-        public void saveToFile() { }
 
         public string readFromFile(string fileName)
         {
-            // TODO Maybe use a try catch when opeing the file?
             string fileContent = "";
-            String line;
-            
-            //Pass the file path and file name to the StreamReader constructor
-            StreamReader stream = new StreamReader(fileName);
-
-            //Read the first line of text
-            line = stream.ReadLine();
-
-            //Continue to read until you reach end of file
-            while (line != null)
+            try
             {
-                //write the line to console window
-                fileContent += line;
-                
-                //Read the next line
+                String line;
+
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader stream = new StreamReader(fileName);
+
+                //Read the first line of text
                 line = stream.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    fileContent += line + "\n";
+
+                    //Read the next line
+                    line = stream.ReadLine();
+                }
+                //close the file
+                stream.Close();
             }
-            //close the file
-            stream.Close();
-            
+            catch (Exception e)
+            {
+                Console.WriteLine("File does not exsist: " + e.Message);
+
+            }
+            finally { } // Not sure if i need something here or not
+
             // Return the file contents as a string
             return fileContent;
         }
