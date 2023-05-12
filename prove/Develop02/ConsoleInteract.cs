8addc47a27c6ed7ecdcf0962ccee24f85ffeda15
Calvinbullock <calvinnbullock@gamil.com>
2023-05-12 12:33:22 -0600

    added MenuInteract() to ConsoleInteract.cs

--- prove/Develop02/ConsoleInteract.cs
+++ prove/Develop02/ConsoleInteract.cs
@@ -8,6 +8,28 @@ namespace Develop02
     ///</summary> 
     class ConsoleInteract
     {
+        public int MenuInteract(Menu menu)
+        {
+            // Lists out menu items and prompts the user to enter a 
+            // choice from the menu.
+            // Paramiter: takes a menu instance
+            // returns: the menu chouice as a int
+
+            // loops trough menuItems and prints each on a new line.
+            List<string> menuItems = menu.GetMenuItems();
+            foreach (string item in menuItems)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Prompts for input
+            Console.Write($"What would you like to do?: ");
+            string entry = Console.ReadLine();
+
+            // converts input to an int then returns the int.
+            return Convert.ToInt32(entry);
+        }
+
         public string EntryInput(string prompt)
         {
             // Prompts the user for input.
