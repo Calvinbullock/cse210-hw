7c3c3df84978819bb772c319d647f72f359c992d
Calvinbullock <calvinnbullock@gamil.com>
2023-05-12 10:38:42 -0600

    added test cases for prompts & FileManager

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -25,10 +25,27 @@ namespace Develop02
             //     Console.WriteLine(message);
             // }
 
-            // Test for prompts class
-            Prompts prompt = new Prompts();
-            string randPrompt = prompt.GetRandomPrompt();
-            Console.WriteLine($"{randPrompt}");
+            // ------------------------------------------------------------------ \\
+            // // Test for prompts class
+
+            // Prompts prompt = new Prompts();
+            // string randPrompt = prompt.GetRandomPrompt();
+            // Console.WriteLine($"{randPrompt}");
+
+            // ------------------------------------------------------------------ \\
+            // // FileManger class test case
+
+            // FileManager fileMan = new FileManager();
+            // string file = "journalEntry.txt";
+            // string content = "If the wighted companion cube talks back ignore it.\nNot that it can talk back.";
+
+            // ------------------------------------------------------------------ \\
+            // FIleManger test case
+
+            // fileMan.SaveToFile(file, content);
+            // string fileContent = fileMan.ReadFromFile(file);
+            // Console.WriteLine($"{fileContent}");
+
         }
     }
 }
