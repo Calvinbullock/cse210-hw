623090f08d96dbcffcea172aa2e14d09baf3e676
Calvinbullock <calvinnbullock@gamil.com>
2023-05-14 09:05:42 -0600

    Load file works!!!! I think

--- prove/Develop02/FileManager.cs
+++ prove/Develop02/FileManager.cs
@@ -59,9 +59,6 @@ namespace Develop02
                 fileContent += line + "\n"; // TODO get ride of \n?
             }
 
-            // Creates new Entry instance
-            Entry yourEntry = new Entry();
-
             // The diffrent charicters that dennote where the fileContent sting should be split
             char[] delimiters = new char[] { ':', '-', '\n' };
 
@@ -75,40 +72,104 @@ namespace Develop02
                 {
                     if (count % 3 == 0)
                     {
-                        Console.WriteLine($"L.79'{index}', '{count}'"); // DEBUG
+                        // Console.WriteLine($"L.79'{index}', '{count}'"); // DEBUG
                         sub.Add(index);
                     }
                     else if (count % 4 == 0)
                     {
-                        Console.WriteLine($"L.84'{index}', '{count}'"); // DEBUG
+                        // Console.WriteLine($"L.84'{index}', '{count}'"); // DEBUG
                         sub.Add(index);
                     }
                     else if (count == 1)
                     {
-                        Console.WriteLine($"L.84'{index}', '{count}'"); // DEBUG
+                        // Console.WriteLine($"L.84'{index}', '{count}'"); // DEBUG
                         sub.Add(index);
                     }
                 }
                 count++;
             }
 
-            // Stores the parsed file content to new entries 
+            // Stores the parsed file content to new entries then to journal
             count = 0;
             foreach (string index in sub)
             {
                 if (count % 3 == 0)
                 {
+                    // Creates new Entry instance
+                    Entry yourEntry = new Entry();
+
                     Console.WriteLine($"L.84'{sub[count]}', '{sub[count + 1]}', '{sub[count + 2]}'"); // DEBUG
-                    yourEntry.Store(sub[count+1], sub[count + 2], sub[count]);
+                    yourEntry.Store(sub[count + 1], sub[count + 2], sub[count]);
+
+                    // Store the entries in journal
+                    Console.WriteLine($"L.105: {yourEntry.GetAsString()}"); // DEBUG
+                    journal.StoreEntry(yourEntry);
                 }
                 count++;
             }
-
-            // Store the entries in journal
-            journal.StoreEntry(yourEntry);
-
             // Return the file contents as a string
             return fileContent;
+        }
+
+        public string ReadFromFile2(string fileName, Journal journal)
+        {
+            string fileContent = "";
+            string[] lines = { };
+            List<string> sub = new List<string>();
+
+            // Open the file or through an error msg
+            try
+            {
+                lines = System.IO.File.ReadAllLines(fileName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File did not open." + e.Message); // DEBUG
+            }
+
+            // Might be redundent loop TODO - Might be better way
+            // Loop through Lines and parse into one long string
+            foreach (string line in lines)
+            {
+                fileContent += line + "\n"; // TODO get ride of \n?
+            }
+
+            // Console.WriteLine($"L.70"); // DEBUG
+            // Parses FileContent into substrings
+            int count = 0;
+            foreach (string index in fileContent.Split(':', StringSplitOptions.RemoveEmptyEntries))
+            {
+                // Console.WriteLine($"L.79'{index}', '{count}'"); // DEBUG
+                sub.Add(index);
+                count++;
+            }
+            // Console.WriteLine($"L.146'{sub.Count}'"); // DEBUG
+
+            // Stores the parsed file content to new entries then to journal
+            count = 0;
+            while (count < sub.Count)
+            {
+                // Console.WriteLine($"\nL.152 '{count}', '{sub[count]}'\n"); // DEBUG
+
+                if (count % 2 == 0 && count % 4 != 0)
+                {
+                    // Creates new Entry instance
+                    Entry yourEntry = new Entry();
+
+                    // Console.WriteLine($"\nL.84 '{count}', '{sub[count - 1]}', '{sub[count]}', '{sub[count - 2]}'\n"); // DEBUG
+                    yourEntry.Store(sub[count - 1], sub[count], sub[count - 2]);
+
+                    // Store the entries in journal
+                    journal.StoreEntry(yourEntry);
+
+                    // To skip the empty entry - TODO BUG
+                    count++;
+                }
+                count++;
+            }
+            // Return the file contents as a string
+            return fileContent;
+
         }
     }
 }
