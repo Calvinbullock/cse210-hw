37175378493b6608abe2079881943575fe021ffd
Calvinbullock <calvinnbullock@gamil.com>
2023-05-16 09:42:19 -0600

    fraction class completed with test cases

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,7 +7,23 @@ namespace Learning02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Learning03 World!");
+
+            // 1
+            Fraction fraction1 = new Fraction(1);
+            Console.WriteLine($"{fraction1.GetFractionString()}");
+            Console.WriteLine($"{fraction1.GetDecimalValue()}");
+            // 5
+            Fraction fraction2 = new Fraction(5);
+            Console.WriteLine($"{fraction2.GetFractionString()}");
+            Console.WriteLine($"{fraction2.GetDecimalValue()}");
+            // 3 / 4
+            Fraction fraction3 = new Fraction(3, 4);
+            Console.WriteLine($"{fraction3.GetFractionString()}");
+            Console.WriteLine($"{fraction3.GetDecimalValue()}");
+            // 1 / 3
+            Fraction fraction4 = new Fraction(1, 3);
+            Console.WriteLine($"{fraction4.GetFractionString()}");
+            Console.WriteLine($"{fraction4.GetDecimalValue()}");
         }
     }
 
