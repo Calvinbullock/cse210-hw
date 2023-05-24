062d11ef96263db5a16adb78dd2415cbaab0bf8a
Calvinbullock <calvinnbullock@gamil.com>
2023-05-24 11:13:49 -0600

    Program.cs Dev03 basic main started

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,12 +1,18 @@
 using System;
+
 namespace Develop03
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Develop03 World!");
+
+            string rawScripture = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy cpaths.";
+
+            Scripture scripture = new Scripture(rawScripture);
+            UserInterface ui = new UserInterface();
+
+            ui.Display(scripture);
         }
     }
 }
