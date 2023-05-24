3334a4164e29729906f0ac8033d804984d81fb75
Calvinbullock <calvinnbullock@gamil.com>
2023-05-24 13:48:57 -0600

    Dev03 more commenting

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,10 +4,11 @@ namespace Develop03
     {
         static void Main(string[] args)
         {
-
+            // Mian content strings
             string scriptRefrance = "Proverbs: 3: 5-6 ";
             string rawScripture = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
 
+            // Creats scripture and UI instances
             Scripture scripture = new Scripture(rawScripture);
             UserInterface ui = new UserInterface();
 
