2fb64db43dd464772d205e0a62612a509323cec9
Calvinbullock <calvinnbullock@gamil.com>
2023-05-29 19:32:11 -0600

    Learning04 update

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -8,17 +8,17 @@ namespace Learning04
         static void Main(string[] args)
         {
             // Create a base "Assignment" object
-            Assignment a1 = new Assignment("James Jimmey", "Algabra");
-            Console.WriteLine(a1.GetSummary());
+            Assignment assign1 = new Assignment("James Jimmey", "Algabra");
+            Console.WriteLine(assign1.GetSummary());
 
             // Now create the derived class assignments
-            MathAssignment a2 = new MathAssignment("Foo Bar", "decimals", "4.6", "4-30");
-            Console.WriteLine(a2.GetSummary());
-            Console.WriteLine(a2.GetHomeworkList());
+            MathAssignment assign2 = new MathAssignment("Foo Bar", "decimals", "4.6", "4-30");
+            Console.WriteLine(assign2.GetSummary());
+            Console.WriteLine(assign2.GetHomeworkList());
 
-            WritingAssignment a3 = new WritingAssignment("Jean Grey", "Mutant History", "Mutant Cures Histry");
-            Console.WriteLine(a3.GetSummary());
-            Console.WriteLine(a3.GetWritingInformation());
+            WritingAssignment assign3 = new WritingAssignment("Jean Grey", "Mutant History", "Mutant Cures Histry");
+            Console.WriteLine(assign3.GetSummary());
+            Console.WriteLine(assign3.GetWritingInformation());
         }
     }
 }
