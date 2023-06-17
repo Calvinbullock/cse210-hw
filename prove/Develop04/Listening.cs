e74c31df6acfe4ddbcab86c24bc2022fe575f17d
Calvinbullock <calvinnbullock@gamil.com>
2023-06-17 09:41:38 -0600

    dev4 Listeing questions added

--- prove/Develop04/Listening.cs
+++ prove/Develop04/Listening.cs
@@ -12,6 +12,15 @@ namespace Develop04
         public Listening(string startMessage)
                     : base(startMessage)
         {
+            _questions = new List<string>
+            {
+                "Who are people that you appreciate?",
+                "What are personal strengths of yours?",
+                "Who are people that you have helped this week?",
+                "When have you felt the Holy Ghost this month?",
+                "Who are some of your personal heroes?"
+            };
+
 
         }
 
