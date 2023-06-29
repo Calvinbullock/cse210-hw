36956c639687ffc96b54f16167858362d37fcf86
Calvinbullock <calvinnbullock@gamil.com>
2023-06-29 12:06:47 -0600

    dev5 lots of work on simpGoal and UI classes

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -2,22 +2,24 @@ namespace Develop05
 {
     class SimpleGoal : Goal
     {
-        private int completionsNeeded;
-        private int completionsTotal;
+        private int _completionsNeeded;
+        private int _achivedCompletions;
+
+        public SimpleGoal(int completionsNeeded, string nameOfGoal, string description, int pointValue) :
+                base(nameOfGoal, description, pointValue)
+        {
+            _completionsNeeded = completionsNeeded;
+        }
 
         public override void RecordProgress()
         {
             // Changes the boolean parameter “completed” / other things depending on child class
         }
 
-        public override void VirtualDisplay()
-        {
-            // Displays a formatted string containing “nameOfGoal” and “description”
-        }
-
         public void PartialPoints()
         {
             // Gives partial points based on how many times they have RecodedProgress()
+
         }
     }
 }
--- prove/Develop05/UserInterface.cs
+++ prove/Develop05/UserInterface.cs
@@ -1,7 +1,94 @@
+using System;
+using System.Collections.Generic;
+
 namespace Develop05
 {
     class UserInterface
     {
+        private List<Goal> _goals;
+
+        public int Menu()
+        {
+            // Console display
+            Console.WriteLine("Menu Items");
+            Console.WriteLine("  1. Create New Goal");
+            Console.WriteLine("  2. List Goals");
+            Console.WriteLine("  3. Save Goals");
+            Console.WriteLine("  4. Load Goals");
+            Console.WriteLine("  5. Record Event");
+            Console.WriteLine("  6. Quit.");
+            Console.Write("Select a choice from the menu: ");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Clear();
+
+            return input;
+        }
+
+        public void ListGoals()
+        {
+            foreach (Goal goal in _goals)
+            {
+                goal.Display();
+            }
+        }
+
+        public void SetNewGoal()
+        {
+            Console.WriteLine("Would you like a:");
+            Console.WriteLine("1. Simple Goal");
+            Console.WriteLine("2. Eternal Goal");
+            Console.Write("Select a choice from the menu: ");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("What is the name of your goal: ");
+            string name = Console.ReadLine();
+
+            Console.Write("What is the short description of your goal: ");
+            string description = Console.ReadLine();
+
+            Console.Write("What is the point association of your goal: ");
+            int points = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Clear();
+
+            // use / build constructers....
+            if (input == 1)
+            {
+                SimpleGoal simpGoal = new SimpleGoal(3, name, description, points);
+                _goals.Add(simpGoal);
+            }
+            else
+            {
+                EteralGoal EGoal = new EteralGoal(name, description, points);
+                _goals.Add(EGoal);
+            }
+        }
+
+        public void SaveGoals(FileManager fileMan)
+        {
+            Console.Write("What would you like to name you goal file: ");
+            string FileName = Console.ReadLine();
+
+            fileMan.SaveToFile(_goals, FileName);
+        }
+
+        public void LoadGoals(FileManager fileMan)
+        {
+            Console.Write("What Goal file would you like to read from: ");
+            string FileName = Console.ReadLine();
+
+            _goals = fileMan.ReadFromFile(FileName);
+        }
+
+        public void RecordEvent()
+        {
+
+        }
 
     }
 }
