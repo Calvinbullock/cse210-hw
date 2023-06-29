027a860012a233b5c976d7cd9ae74a8f4affda84
Calvinbullock <calvinnbullock@gamil.com>
2023-06-29 12:08:29 -0600

    dev5 FileMan class refactoring

--- prove/Develop05/FileManager.cs
+++ prove/Develop05/FileManager.cs
@@ -11,32 +11,47 @@ namespace Develop05
     class FileManager
     {
         public FileManager() { }
-        public void SaveToFile(string fileName, string fileContent)
+        public void SaveToFile(List<Goal> goals, string fileName)
         {
-            // This function wirtes a string to a file
-            // Paramiters:
-            // String: the name of the file you want to write to
-            // String the content you want to save to the file
+            /* =======================================================*\
+            ||This function wirtes a list of goals to a file.         ||
+            ||                                                        ||
+            || Paramiters:                                            ||
+            ||      Goal: the goal that will be saved to a file.      ||
+            ||      String: the name of the file you want to write to.||
+            \*========================================================*/
 
-            try
+            // loops throught list to dump it all to one file
+            foreach (Goal goal in goals)
             {
-                // Open the File
-                StreamWriter stream = new StreamWriter(fileName, true, Encoding.ASCII);
+                string Name = goal.GetNameOfGoal();
+                string description = goal.GetGoalDescription();
 
-                // Writes content to file
-                stream.WriteLine(fileContent);
+                try
+                {
+                    // Open the File
+                    StreamWriter stream = new StreamWriter(fileName, true, Encoding.ASCII);
 
-                // Close the file
-                stream.Close();
+                    // Writes content to file
+                    stream.WriteLine(description);
+
+                    // TODO -----
+                    // TODO -----
+                    // Need to formate how all the goals are dumped to the file
+                    // TODO -----
+
+                    // Close the file
+                    stream.Close();
+                }
+                catch (Exception except)
+                {
+                    Console.WriteLine("Exception: " + except.Message); // TODO Not sure on error message yet..
+                }
+                finally { } // Not sure if i need something here or not
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message); // TODO Not sure on error message yet..
-            }
-            finally { } // Not sure if i need something here or not
         }
 
-        public string ReadFromFile(string fileName)
+        public List<Goal> ReadFromFile(string fileName)
         {
             string fileContent = "";
             string[] lines = { };
@@ -47,13 +62,13 @@ namespace Develop05
             {
                 lines = System.IO.File.ReadAllLines(fileName);
             }
-            catch (Exception e)
+            catch (Exception except)
             {
-                Console.WriteLine("File did not open." + e.Message);
+                Console.WriteLine("File did not open." + except.Message);
             }
 
             // Might be redundent loop TODO - Might be better way
-            // Loop through Lines and parse into one long string
+            //      Loop through Lines and parse into one long string
             foreach (string line in lines)
             {
                 fileContent += line + "\n"; // TODO get ride of \n?
@@ -72,8 +87,8 @@ namespace Develop05
             while (count < subStr.Count)
             {
                 // Breaks sub up into groups of 3 starting with 0 
-                // Logic is every odd that is not direcly divisable by 4 is the last 
-                // in teh groupe of three
+                //      Logic is every odd that is not direcly divisable by 4 is the last 
+                //      in teh groupe of three
                 if (count % 2 == 0 && count % 4 != 0)
                 {
 
