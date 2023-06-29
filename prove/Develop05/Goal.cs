2808d6a900ab81a615a32292a131c56b6018719f
Calvinbullock <calvinnbullock@gamil.com>
2023-06-29 12:07:33 -0600

    dev5 goal class constructer and overall clean up

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,12 +1,18 @@
 namespace Develop05
 {
-    class Goal
+    abstract class Goal
     {
-        //private List<string>;
-        private string nameOfGoal;
-        private string description;
-        private bool completed;
-        private int pointValue;
+        private string _nameOfGoal;
+        private string _description;
+        private int _pointValue;
+        private string _completed = " ";
+
+        public Goal(string nameOfGoal, string description, int pointValue)
+        {
+            _description = description;
+            _pointValue = pointValue;
+            _nameOfGoal = nameOfGoal;
+        }
 
         public void AddGoal()
         {
@@ -18,9 +24,20 @@ namespace Develop05
             // Changes the boolean parameter “completed” / other things depending on child class
         }
 
-        public virtual void VirtualDisplay()
+        public string Display()
         {
             // Displays a formatted string containing “nameOfGoal” and “description”
+            return $"[{_completed}] {_nameOfGoal} ({_description})";
+        }
+
+        public string GetNameOfGoal()
+        {
+            return _nameOfGoal;
+        }
+
+        public string GetGoalDescription()
+        {
+            return _description;
         }
     }
 }
