071162c3d9da2399dd45e0129759415d6aea7b7c
Calvinbullock <calvinnbullock@gamil.com>
2023-06-29 12:05:33 -0600

    Dev5 EGoal class constructer

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -2,15 +2,12 @@ namespace Develop05
 {
     class EteralGoal : Goal
     {
+        public EteralGoal(string nameOfGoal, string description, int pointValue) :
+                base(nameOfGoal, description, pointValue)
+        { }
         public override void RecordProgress()
         {
             // Changes the boolean parameter “completed” / other things depending on child class
         }
-
-        public override void VirtualDisplay()
-        {
-            // Displays a formatted string containing “nameOfGoal” and “description”
-        }
-
     }
 }
