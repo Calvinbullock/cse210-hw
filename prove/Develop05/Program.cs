4d181c76b8e059b550fef5edb411ab118cea5d94
Calvinbullock <calvinnbullock@gamil.com>
2023-06-29 12:08:05 -0600

    dev5 program  class bones

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,12 +1,48 @@
-using System;
-
 namespace Develop05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Develop05 World!");
+            // ----------------- User Interface class instantiation ----------------- \\
+            UserInterface UI = new UserInterface();
+
+            // ----------------- File Manager class instantiation ----------------- \\
+            FileManager FileMan = new FileManager();
+
+            // ----------------- Program Run ----------------- \\
+            int exit = 0;
+            while (exit == 0)
+            {
+                int actionChoice = UI.Menu();
+
+                // Launches the chosen activity
+                switch (actionChoice)
+                {
+                    case 1: // Create New Goal
+                        UI.SetNewGoal();
+                        break;
+
+                    case 2: // List Goals
+                        UI.ListGoals();
+                        break;
+
+                    case 3: // Save Goals
+                        UI.SaveGoals(FileMan);
+                        break;
+
+                    case 4: // Load Goals
+                        UI.LoadGoals(FileMan);
+                        break;
+
+                    case 5: // Record Event
+                        UI.RecordEvent();
+                        break;
+
+                    case 6: // quit
+                        break;
+                }
+            }
         }
     }
 }
