a4249d02c48d64a3c03800c59ecd3eedc13f02ce
Calvinbullock <calvinnbullock@gamil.com>
2023-07-08 21:21:22 -0600

    fin - program.cs basic game loop

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -6,7 +6,41 @@ namespace final
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello FinalProject World!");
+            // ----------------- User Interface class instantiation ----------------- \\
+            UserInterface UI = new UserInterface();
+
+            bool exit = false;
+            int userAction = UI.MainMenu(playerCharName1, playerCharName2);
+
+            while (exit == false)
+            {
+                // Launches the chosen activity
+                switch (userAction)
+                {
+                    case 1: // Start Game
+                        UI.BattleScene();
+                        break;
+
+                    case 2: // Set player 1
+                        player1 = UI.CharicterSelection(_chars);
+                        playerCharName1 = player1.GetName();
+                        break;
+
+                    case 3: // Set player 2
+                        player2 = UI.CharicterSelection(_chars);
+                        playerCharName2 = player2.GetName();
+                        break;
+
+                    case 4: // Record Event
+                        exit = true;
+                        UI.GoodByeMsg();
+                        break;
+
+                    case 5: // Return to mainMenu
+                        userAction = UI.MainMenu(playerCharName1, playerCharName2);
+                        break;
+                }
+            }
         }
     }
 }
