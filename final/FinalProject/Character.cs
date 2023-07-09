9c973d5cb709294e45d34f3f6412287b5c912f34
Calvinbullock <calvinnbullock@gamil.com>
2023-07-08 21:19:58 -0600

    fin - base charc.cs basic atrabutes and functions

--- final/FinalProject/Character.cs
+++ final/FinalProject/Character.cs
@@ -3,11 +3,32 @@ namespace final
 {
     class Charicter
     {
-        private int PowerLevel;
-        private int HealthLevel;
-        private int AttackSpeed;
+        private string _charicterName;
+        private string _charicterDescription;
+        private int _powerLevel;
+        private int _healthLevel;
+        private int _attackSpeed;
+
+        public Charicter(string charicterName, string charicterDescription, int powerLevel, int healthLevel, int attackSpeed)
+        {
+            _charicterName = charicterName;
+            _charicterDescription = charicterDescription;
+            _powerLevel = powerLevel;
+            _healthLevel = healthLevel;
+            _attackSpeed = attackSpeed;
+        }
 
         public void UpdateHealth(int damage) { }
         public void UpdatePower(int powerUpdate) { }
+
+        public string GetName()
+        {
+            return _charicterName;
+        }
+
+        public string GetCharicterSummery()
+        {
+            return _charicterDescription;
+        }
     }
 }
