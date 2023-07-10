82aaf4750af9f797e48178ecd9d3419aa8a108f0
Calvinbullock <calvinnbullock@gamil.com>
2023-07-10 15:55:12 -0600

    Fin - Fireball commeting

--- final/FinalProject/Action.cs
+++ final/FinalProject/Action.cs
@@ -3,28 +3,43 @@ namespace final
 {
     abstract class Action
     {
+        private string _name;
         private int _damageToInstigater;
         private int _damageToReciver;
 
-        public Action(int damageToInstigater, int damageToReciver)
+        public Action(string name, int damageToInstigater, int damageToReciver)
         {
             _damageToInstigater = damageToInstigater;
             _damageToReciver = damageToReciver;
+            _name = name;
         }
 
         public abstract string ApplyActions(Charicter instigater, Charicter reciver);
+        /*========================================================*\
+        || This is the abstract for all actions.                  ||
+        ||                                                        ||
+        || Paramiters:                                            ||
+        ||      instigater: 
+        ||      reciver: 
+        \*========================================================*/
         // TODOs Maybe overload for diffrent actions - not nailed down 
         //      yet...
-        // TODOs Figure out some way to printthe details of the move 
+        // TODOs Figure out some way to print the details of the move 
         //      during the battle
 
         public int GetDamageToInstgater()
         {
             return _damageToInstigater;
         }
+
         public int GetDamageToReciver()
         {
             return _damageToReciver;
+        }
+
+        public string GetName()
+        {
+            return _name;
         }
     }
 }
--- final/FinalProject/FireBall.cs
+++ final/FinalProject/FireBall.cs
@@ -1,16 +1,32 @@
 
 namespace final
 {
-    abstract class FireBall : Action
+    class FireBall : Action
     {
-        public FireBall(int damageToInstigater, int damageToReciver)
-                : base(damageToInstigater, damageToReciver)
+        public FireBall(string name, int damageToInstigater, int damageToReciver)
+                : base(name, damageToInstigater, damageToReciver)
         {
+            /*========================================================*\
+            || This is the abstract for all actions, posstive for     ||
+            ||      adding health pos numbers, dealing damage         ||
+            ||      negativs.                                         ||
+            ||                                                        ||
+            || Paramiters:                                            ||
+            ||      damageToInstigater:                               ||
+            ||      damageToReciver:                                  ||
+            \*========================================================*/
 
         }
 
         public override string ApplyActions(Charicter instigater, Charicter reciver)
         {
+            /*========================================================*\
+            || This is the abstract for all actions.                  ||
+            ||                                                        ||
+            || Paramiters:                                            ||
+            ||      instigater: 
+            ||      reciver: 
+            \*========================================================*/
             instigater.UpdateHealth(this.GetDamageToReciver());
             return $"{instigater.GetName()} used FireBall 20 Dmg delt";
         }
