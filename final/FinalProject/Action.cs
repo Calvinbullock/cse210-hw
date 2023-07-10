2a6ddf90674f0bb8aa0ce09dc1a18e9265d53ba7
Calvinbullock <calvinnbullock@gamil.com>
2023-07-10 09:24:31 -0600

    fin - action.cs completed

--- final/FinalProject/Action.cs
+++ final/FinalProject/Action.cs
@@ -3,7 +3,13 @@ namespace final
 {
     abstract class Action
     {
-        public Action(Charicter Instigater, Charicter Reciver) { }
+        public Charicter _instigater;
+        public Charicter _reciver;
+        public Action(Charicter instigater, Charicter reciver)
+        {
+            _instigater = instigater;
+            _reciver = reciver;
+        }
 
         public abstract void ApplyActions();
         // Maybe overload for diffrent actions - not nailed down yet...
